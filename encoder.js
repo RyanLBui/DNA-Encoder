71c016b419a037d4590dbfc9c3c192222460e4cb
RyanLBui <42982855+RyanLBui@users.noreply.github.com>
2019-10-14 14:33:13 -0500

    longest common subsequence

--- encoder.js
+++ encoder.js
@@ -120,20 +120,137 @@ function getPrimaryStrand()
     primaryToBinary(input);
 }
 
+// turn primary strand to binary, then turn from binary to ascii
 function primaryToBinary(primaryStrand)
 {
-    var chars = {'A':'00','T':'01','C':'11', 'G':'10'};
     var  output = document.getElementById("primaryOutputBox");
+
+    // replace dna letters with binary numbers
+    var chars = {'A':'00','T':'01','C':'11', 'G':'10'};
     primaryStrand = primaryStrand.replace(/[ATGC]/g, m => chars[m]);
     console.log(primaryStrand);
 
+    // binary number to ascii
     primaryStrand = primaryStrand.replace(/(\w{8})/g, '$1 ').replace(/(^\s+|\s+$)/,'');
     var binString = '';
-
     primaryStrand.split(" ").map(function(bin) 
     {
         binString += String.fromCharCode(parseInt(bin, 2));
     });
-
+    
     output.value = binString;
+}
+
+function getDnaStrings()
+{
+    var dna1 = document.getElementById("lcsInputbox1").value;
+    var dna2 = document.getElementById("lcsInputbox2").value;
+
+    //call lcs function
+    var array;
+    longestCommonSubsequence(dna1, dna2);
+}
+
+function commonSubsequence(dna1, dna2)
+{
+    var output = document.getElementById("subsequenceOutputBox");
+    
+    var dna1Substring = dna1.substr(0, dna1.length - 1);
+    var dna2Substring = dna2.substr(0, dna2.length - 1);
+ 
+    if (dna1.length == 0 || dna2.length == 0) 
+    {
+        output.value = '';
+    } 
+    else if (dna1.charAt(dna1.length - 1) === dna2.charAt(dna2.length - 1)) 
+    {
+        return commonSubsequence(dna1Substring, dna2Substring) + dna1.charAt(dna1.length - 1);
+    } 
+    else 
+    {
+        var x = commonSubsequence(dna1, dna2Substring);
+        var y = commonSubsequence(dna1Substring, dna2);
+        output.value = (x.length > y.length) ? x : y;
+    }
+}
+
+function longestCommonSubsequence(set1, set2) 
+{
+    var output = document.getElementById("subsequenceOutputBox");
+    // Init LCS matrix.
+    const lcsMatrix = Array(set2.length + 1).fill(null).map(() => Array(set1.length + 1).fill(null));
+
+    // Fill first row with zeros.
+    for (let columnIndex = 0; columnIndex <= set1.length; columnIndex += 1) 
+    {
+        lcsMatrix[0][columnIndex] = 0;
+    }
+
+    // Fill first column with zeros.
+    for (let rowIndex = 0; rowIndex <= set2.length; rowIndex += 1) 
+    {
+        lcsMatrix[rowIndex][0] = 0;
+    }   
+
+    // Fill rest of the column that correspond to each of two strings.
+    for (let rowIndex = 1; rowIndex <= set2.length; rowIndex += 1) 
+    {
+        for (let columnIndex = 1; columnIndex <= set1.length; columnIndex += 1) 
+        {
+            if (set1[columnIndex - 1] === set2[rowIndex - 1]) 
+            {
+                lcsMatrix[rowIndex][columnIndex] = lcsMatrix[rowIndex - 1][columnIndex - 1] + 1;
+            } 
+            else    
+            {
+                lcsMatrix[rowIndex][columnIndex] = Math.max(
+                lcsMatrix[rowIndex - 1][columnIndex],
+                lcsMatrix[rowIndex][columnIndex - 1],
+                );
+            }
+    }
+}
+
+    // Calculate LCS based on LCS matrix.
+    if (!lcsMatrix[set2.length][set1.length]) 
+    {
+        // If the length of largest common string is zero then return empty string.
+        //return [''];
+        output.value = "No common subsequence";
+    }
+
+    const longestSequence = [];
+    let columnIndex = set1.length;
+    let rowIndex = set2.length;
+
+    while (columnIndex > 0 || rowIndex > 0) 
+    {
+        if (set1[columnIndex - 1] === set2[rowIndex - 1]) 
+        {
+            // Move by diagonal left-top.
+            longestSequence.unshift(set1[columnIndex - 1]);
+            columnIndex -= 1;
+            rowIndex -= 1;
+        } 
+        else if (lcsMatrix[rowIndex][columnIndex] === lcsMatrix[rowIndex][columnIndex - 1]) 
+        {
+            // Move left.
+            columnIndex -= 1;
+        } 
+        else    
+        {
+            // Move up.
+            rowIndex -= 1;
+        }
+    }
+    
+    if(longestSequence.length < 1)
+    {
+        output.value = "No common subsequence";
+    }
+    else
+    {
+        output.value = longestSequence.join("");
+    }
+    
 }
